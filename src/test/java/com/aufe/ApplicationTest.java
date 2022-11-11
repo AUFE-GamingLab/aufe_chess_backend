63c30425b605b4f1986b706b9ca27aea7c815593
asyncio886 <2409298768@qq.com>
2022-11-11 19:34:11 +0800

    SpringBoot项目创建成功

---
+++ src/test/java/com/aufe/ApplicationTest.java
@@ -0,0 +1,11 @@
+package com.aufe;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest(classes = AufeChessApplication.class)
+@RunWith(SpringRunner.class)
+public class ApplicationTest {
+
+}
